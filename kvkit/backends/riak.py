ce3447dfa3e7e10cc7e2ba4cd20fe3f07730f26f
Shuhao Wu <shuhao@shuhaowu.com>
2013-11-23 19:37:11 -0500

    Fixed license header

--- kvkit/backends/riak.py
+++ kvkit/backends/riak.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 # This file is part of kvkit
 #
-# Riakkit or Leveldbkit is free software: you can redistribute it and/or modify
+# kvkit is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Riakkit or Leveldbkit is distributed in the hope that it will be useful,
+# kvkit is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with Riakkit or Leveldbkit. If not, see <http://www.gnu.org/licenses/>.
+# along with kvkit. If not, see <http://www.gnu.org/licenses/>.
 
 """This backend uses Riak to store data.
 
@@ -27,7 +27,7 @@ def clear_document(self, **args):
   pass
 
 
-def delete(cls, key, **args):
+def delete(cls, key, doc=None, **args):
   pass
 
 
@@ -59,5 +59,5 @@ def list_all_keys(cls, start_value=None, end_value=None, **args):
   pass
 
 
-def save(cls, key, data, **args):
+def save(self, key, data, **args):
   pass
